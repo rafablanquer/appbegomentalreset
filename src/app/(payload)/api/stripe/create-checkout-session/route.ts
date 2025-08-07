4e2399cfe98f7a184dfc5a016b57ca3899dfd1e3
Rafa Blanquer <rbr@rafablanquer.es>
2025-08-07 11:53:11 +0200

    .

--- src/app/(payload)/api/stripe/create-checkout-session/route.ts
+++ src/app/(payload)/api/stripe/create-checkout-session/route.ts
@@ -7,6 +7,8 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
     apiVersion: '2025-07-30.basil',
 })
 
+
+
 export async function POST(req: NextRequest) {
     try {
         const payload = await getPayload({ config })
